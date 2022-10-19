70113fb5b8e825c970adf9fd985a6142be2d318a
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-20 00:24:39 +0300

    Adds fetch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './css/styles.css';
+import fetchItems from './js/fetchItems';
 import Notiflix from 'notiflix';
 import cardTemplate from './js/templates/cardTemplate.hbs';
-import fetchItems from './js/fetchItems';
 
-console.log('hi index js');
+const searchTextRef = document.querySelector('.search-form');
+const searchBtnRef = document.querySelector('.search-button');
+
+// console.log(searchTextRef);
+// console.log(searchBtnRef);
+
+searchTextRef.addEventListener('input', oninput);
+searchBtnRef.addEventListener('submit', onSubmit);
+
+function oninput(e) {
+  console.log(e);
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log(e);
+}
+
+// function renderMarkup(e) {}
--- src/js/fetchItems.js
+++ src/js/fetchItems.js
@@ -1,1 +1,15 @@
-console.log('hi fetch');
+const axios = require('axios').default;
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '30715503-b05874fb24d95ac5a3c3e4a16';
+
+export async function getUser(text) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=${KEY}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
