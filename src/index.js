09fe557da48263fa97c5154061bac299cd525e7e
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-20 00:41:59 +0300

    Removes extra comments

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import Notiflix from 'notiflix';
 import cardTemplate from './js/templates/cardTemplate.hbs';
 
 const searchTextRef = document.querySelector('.search-form');
-const searchBtnRef = document.querySelector('.search-button');
-
-// console.log(searchTextRef);
-// console.log(searchBtnRef);
+const searchBtnRef = document.querySelector('#search-button');
 
 searchTextRef.addEventListener('input', oninput);
 searchBtnRef.addEventListener('submit', onSubmit);
@@ -20,5 +17,3 @@ function onSubmit(e) {
   e.preventDefault();
   console.log(e);
 }
-
-// function renderMarkup(e) {}
