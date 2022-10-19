d78465547e4cba09820023ae0330138467f4e763
Oleksandr Vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-10-20 01:42:55 +0300

    Adds main functions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import './css/styles.css';
-import fetchItems from './js/fetchItems';
+// import fetchItems from './js/fetchItems';
 import Notiflix from 'notiflix';
-import cardTemplate from './js/templates/cardTemplate.hbs';
+import card from './js/templates/card.hbs';
 
 const searchTextRef = document.querySelector('.search-form');
 const searchBtnRef = document.querySelector('#search-button');
+const galleryListRef = document.querySelector('.gallery-list');
 
 searchTextRef.addEventListener('input', oninput);
 searchBtnRef.addEventListener('submit', onSubmit);
 
+const axios = require('axios').default;
+
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '30715503-b05874fb24d95ac5a3c3e4a16';
+
+async function getItems(searchedText) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=${KEY}&q=${searchedText}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 function oninput(e) {
-  console.log(e);
+  const searchText = e.target.value.trim().toLowerCase();
 }
 
 function onSubmit(e) {
   e.preventDefault();
-  console.log(e);
+  getItems(searchText).then(renderMarkup).catch(onError);
 }
 
-function renderMarkup() {}
+function renderMarkup(foundData) {
+  galleryListRef.insertAdjacentHTML('beforeend', card(foundData));
+}
+
+function onError() {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
--- src/js/fetchItems.js
+++ src/js/fetchItems.js
@@ -1,15 +1,15 @@
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '30715503-b05874fb24d95ac5a3c3e4a16';
+// const BASE_URL = 'https://pixabay.com/api/';
+// const KEY = '30715503-b05874fb24d95ac5a3c3e4a16';
 
-export async function getUser(text) {
-  try {
-    const response = await axios.get(
-      `${BASE_URL}?key=${KEY}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true`
-    );
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function fetchItems(text) {
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}?key=${KEY}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true`
+//     );
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
